6bbd9c13e3be4a7d82cb4e25ee3012317b8e1de8
Noah <fordgnoah@gmail.com>
2025-09-30 17:03:25 -0500

    Added .gitignore &Modified helloWorld.c

--- helloWorld.c
+++ helloWorld.c
@@ -2,7 +2,8 @@
 
 int main() {
 
-    printf("Hello, World!");
+    printf("Hello, World!"); //text in a print function must be encapsulated in ""
+    printf("\nShopping List:\nChicken Noodle Soup"); // \n can be used to move text to the next line
 
     return 0;
 }
