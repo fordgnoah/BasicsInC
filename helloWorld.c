b8ac5a162da79586aadb904add71af012d4a7f20
Noah <fordgnoah@gmail.com>
2025-09-30 17:24:01 -0500

    Enhance comments in helloWorld.c for clarity and understanding

--- helloWorld.c
+++ helloWorld.c
@@ -3,7 +3,14 @@
 int main() {
 
     printf("Hello, World!"); //text in a print function must be encapsulated in ""
-    printf("\nShopping List:\nChicken Noodle Soup"); // \n can be used to move text to the next line
+    printf("\nShopping List:\nChicken Noodle Soup"); // \n can be used to move text to the next line, while \n \n creates a blank line
+
+    int num = 3;
+    printf("%d", num); //printing variables requires format identifiers
+    //%d = integer
+    //%c = char
+    //%f = float
+
 
     return 0;
 }
